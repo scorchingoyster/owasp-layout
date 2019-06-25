873dedf3d50b23581675da7dd4c6e0e1433f74f6
scorchingoyster <hiroko@DESKTOP-RCTCV3S.localdomain>
2019-06-25 17:57:20 -0400

    refactor CSS files

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,8 +1,12 @@
+// toggle mobile menu through CSS class toggle
 function toggleMenu() {
   var x = document.getElementById("menu-toggle");
+  var y = document.getElementById("overlay");
   if (x.classList.contains("hide-el")) {
     x.className = "mobile-menu";
+    y.className = "";
   } else {
     x.className = "mobile-menu hide-el";
+    y.className = "hide-el";
   }
 }
