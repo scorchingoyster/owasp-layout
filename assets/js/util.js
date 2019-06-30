c1cf675a7534d6aa5a7b339ae3e16622cc1bb974
scorchingoyster <>
2019-06-30 13:09:50 -0400

    complete tab switching script

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,7 +1,7 @@
 // element hiding CSS utility class
 var el = "hide-el";
 
-// toggle mobile menu through CSS class toggle
+// toggle mobile menu through toggling the class above
 function toggleMenu() {
   var x = document.getElementById("menu-toggle");
   var y = document.getElementById("overlay");
@@ -20,11 +20,23 @@ function switchTab(event, tabName) {
 
   var i, tab, tab_link;
 
+  // hide all tab content by default
   tab = document.getElementsByClassName("tab");
   for (i = 0; i < tab.length; i++) {
     tab[i].classList.add(el);
   }
 
+  tab_link = document.getElementsByClassName("tab_link");
+  for (i = 0; i < tab_link.length; i++) {
+    tab_link[i].classList.remove("current");
+  }
+  
   document.getElementById(tabName).classList.remove(el);
+  event.currentTarget.classList.add("current");
 
 }
+
+// simulate click behavior to show first tab by default
+document.addEventListener("DOMContentLoaded", function(){
+  document.getElementById("tab_default").click();
+});
