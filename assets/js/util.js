68eb678e989f784e45da91720d1774632ff2394c
scorchingoyster <>
2019-06-30 12:46:38 -0400

    add tab switching script

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,12 +1,30 @@
+// element hiding CSS utility class
+var el = "hide-el";
+
 // toggle mobile menu through CSS class toggle
 function toggleMenu() {
   var x = document.getElementById("menu-toggle");
   var y = document.getElementById("overlay");
-  if (x.classList.contains("hide-el")) {
-    x.className = "mobile-menu";
-    y.className = "";
+  if (x.classList.contains(el)) {
+    x.classList.remove(el);
+    y.classList.remove(el);
   } else {
-    x.className = "mobile-menu hide-el";
-    y.className = "hide-el";
+    x.classList.add(el);
+    y.classList.add(el);
   }
+}
+
+// tab switcher
+function switchTab(event, tabName) {
+  event.preventDefault();
+
+  var i, tab, tab_link;
+
+  tab = document.getElementsByClassName("tab");
+  for (i = 0; i < tab.length; i++) {
+    tab[i].classList.add(el);
+  }
+
+  document.getElementById(tabName).classList.remove(el);
+
 }
