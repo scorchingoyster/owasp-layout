0bb770f354a525bb63c8b9bfe943573690a232cb
scorchingoyster <>
2019-07-10 11:04:31 -0400

    fix browser update notification and file naming

--- assets/js/util.js
+++ assets/js/util.js
@@ -47,16 +47,15 @@ $(document).ready(function() {
     $(this).children(".dropdown-menu").toggleClass(hideEl);
   });
 
-
-  // browser version checker and notifier
-  // source: https://browser-update.org
-  var $buoop = {required:{e:-2,f:-3,o:-3,s:-1,c:-3},insecure:true,unsupported:true,api:2019.06 }; 
-  function $buo_f(){ 
-   var e = document.createElement("script"); 
-   e.src = "//browser-update.org/update.min.js"; 
-   document.body.appendChild(e);
-  };
-  try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-  catch(e){window.attachEvent("onload", $buo_f)}
-
 });
+
+// browser version checker and notifier
+// source: https://browser-update.org
+var $buoop = {required:{e:-1,f:-3,o:-3,s:-1,c:-3},insecure:true,reminder:0,api:2019.07 }; 
+function $buo_f(){ 
+ var e = document.createElement("script"); 
+ e.src = "//browser-update.org/update.min.js"; 
+ document.body.appendChild(e);
+};
+try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+catch(e){window.attachEvent("onload", $buo_f)}
